9a8e4059997dcd0adbf2e6bf24a4c701d14afcbb
Daniel Koch <koch@triple6.org>
2023-08-16 13:48:34 +0200

    hacky way to patch/remove hard depency on libpcap.so.0.8 post build

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -2,10 +2,12 @@
 
 set -eo pipefail
 
-sudo apt-get update && sudo apt-get install -y libpcap-dev
+sudo apt-get update && sudo apt-get install -y libpcap-dev patchelf
 
 go build -ldflags "-s -w -X main.version=$GITHUB_REF_NAME" albiondata-client.go
 ls -la
+
+patchelf --replace-needed libpcap.so.0.8 libpcap.so albiondata-client
 
 gzip -9 albiondata-client
 mv albiondata-client.gz update-linux-amd64.gz
